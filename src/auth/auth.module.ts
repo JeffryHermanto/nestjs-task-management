243a40b5a17004f9119e223449184bb127c1fed3
Jeffry Hermanto <jeffryhermanto@gmail.com>
2023-07-05 20:09:08 +0700

    refactor(config): Implement Configuration Management

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,15 +7,21 @@ import { UsersRepository } from './users.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStratey } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: {
-        expiresIn: 3600, // 1 hour
-      },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
     }),
     TypeOrmModule.forFeature([User]),
   ],
